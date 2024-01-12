94f9b0a7d72e38c668e75145d0b63d406d2028ed
Nicholas-Battista <battistan23@gmail.com>
2024-01-11 17:32:42 -0700

    created functions for numbers, delete, and clear
    created event listners in js to read in the buttons from html, then on
    a click preform the action associated with the button. so the calc
    screen can now take in numbers, clear them, and delete them.
    
    additional small css styles as well

---
+++ script.js
@@ -0,0 +1,31 @@
+const numberInput = Array.from(document.querySelectorAll('.num'));
+let screenText = document.querySelector('.screenText');
+const CLEAR = document.querySelector('.clear');
+const DELETE = document.querySelector('.delete');
+
+
+numberInput.forEach(btn => {
+    btn.addEventListener("click", () => {
+        const clickedNumber = btn.innerHTML;
+
+        if (screenText.innerHTML === '0') {
+            screenText.innerHTML = clickedNumber;
+        }
+        else {
+            screenText.innerHTML = screenText.innerHTML.concat(clickedNumber);
+        }
+    });
+});
+
+CLEAR.addEventListener('click', () => {
+    screenText.innerHTML = '0';
+});
+
+DELETE.addEventListener('click', () => {
+    let modifiedText = screenText.innerHTML.split('').slice(0, -1).join('');
+    screenText.innerHTML = modifiedText;
+
+    if (screenText.innerHTML.length === 0) {
+        screenText.innerHTML = '0';
+    }
+});
