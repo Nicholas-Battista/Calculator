79fbbd582cedc36c70bd11b23525873828ed3cb2
Nicholas-Battista <battistan23@gmail.com>
2024-01-11 19:47:48 -0700

    created eventlistner for operations
    js eventlistner attatched to an array of the op
    buttons waits for a op button to be clicked, and
    then displays the previous number and op in the
    line above on the screen. way to track each operation preformed

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const numberInput = Array.from(document.querySelectorAll('.num'));
 let screenText = document.querySelector('.screenText');
+let runningText = document.querySelector('.running');
+const operators = Array.from(document.querySelectorAll('.op'));
 const CLEAR = document.querySelector('.clear');
 const DELETE = document.querySelector('.delete');
 const DECIMAL = document.querySelector('.decimal');
+
+let result;
 
 
 numberInput.forEach(btn => {
@@ -18,8 +22,15 @@ numberInput.forEach(btn => {
     });
 });
 
+operators.forEach(operator => {
+    operator.addEventListener('click', () => {
+        runningText.innerHTML = screenText.innerHTML + " " + operator.innerHTML;
+    })
+});
+
 CLEAR.addEventListener('click', () => {
     screenText.innerHTML = '0';
+    runningText.innerHTML = '';
 });
 
 DELETE.addEventListener('click', () => {
@@ -36,3 +47,21 @@ DECIMAL.addEventListener('click', () => {
         screenText.innerHTML = screenText.innerHTML.concat('.');
     }
 });
+
+function add(a, b){
+   return result = a + b;
+}
+
+function subtract(a, b){
+    return result = a - b;
+}
+
+function multiply(a, b){
+    return result = a * b;
+}
+
+function divide(a, b){
+    return result = a / b;
+}
+
+// upon click of any of the operators need to send the screentexthtml to a new p above it with the operator next to it, and store the initial value of the innerhtml in var a, then on next click of number have that override the innerhtml, then store second number in var b.
